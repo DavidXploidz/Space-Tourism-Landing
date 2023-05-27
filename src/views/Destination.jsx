fb8fbb80935e8a532d7fdef660f8e27a40477d77
DavidXploidz <david.adame.itd@gmail.com>
2023-05-26 23:53:40 -0600

    ajustes visuales y espaciado en destinations

--- src/views/Destination.jsx
+++ src/views/Destination.jsx
@@ -1,15 +1,53 @@
 import '../styles/Destination.css'
+import { useState, useEffect } from 'react'
 
 export default function Destination() {
+
+  useEffect(() => {
+    consultarApi()
+  }, []);
+  
+
+  const [data, setData] = useState([]);
+  const [planet, setPlanet] = useState('')
+
+  const consultarApi = async () => {
+    const url = 'http://localhost:5173/db.json';
+    const respuesta = await fetch(url);
+    const resultado = await respuesta.json();
+    setData(resultado.destinations);
+    setPlanet(resultado.destinations[0]);
+  }
+
+  const {name, description, distance, travel } = planet;
+
   return (
     <div className='destination'>
       <h2 className='destination__title'><span className='destination__title destination__title--span'>01</span>Pick your destination</h2>
       <div className='destination__grid'>
-        <div>
+        <div className='destination__image'>
             <img src="/images/destination/image-moon.png" alt="image moon" />
         </div>
-        <div>
-            2
+        <div className='destination__content'>
+          <ul className='destination__ul'>
+            {data.map(planet => (
+              <li key={planet.name} className='destination__li'>{planet.name}</li>
+            ))}
+          </ul>
+          <div>
+            <h2 className='destination__name'>{name}</h2>
+            <p className='destination__description'>{description}</p>
+          </div>
+          <div className='destination__extras'>
+            <div>
+              <p className='destination__text'>avg. distance</p>
+              <p className='destination__numbers'>{distance}</p>
+            </div>
+            <div>
+              <p className='destination__text'>est. travel time</p>
+              <p className='destination__numbers'>{travel}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
