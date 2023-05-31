8a41901bbc5dac91804504c471b4c85dc7e710a5
DavidXploidz <david.adame.itd@gmail.com>
2023-05-30 23:38:04 -0600

    barra de navegacion ajustada segun su estado usando useLocation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
 import '../styles/Header.css'
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
 export default function Header() {
 
+  const location = useLocation();
+  const currentPath = location.pathname;
 
+  // Extraer el nombre de la ruta
+  const routeName = currentPath.substring(1); // Eliminar la barra inicial si es necesario
 
 
   return (
@@ -12,10 +17,10 @@ export default function Header() {
       </picture>
       <nav className='header__nav'>
         <ul className='header__links'>
-          <Link className='header__link' to="/home">00 <span className='header__link--normal'>home</span></Link>
-          <Link className='header__link' to="/destination">01 <span className='header__link--normal'>destination</span></Link>
-          <Link className='header__link' to="/crew">02 <span className='header__link--normal'>crew</span></Link>
-          <Link className='header__link' to="/technology">03 <span className='header__link--normal'>technology</span></Link>
+          <Link className={`${routeName === 'home' ? 'header__link--active' : ''} header__link`} to="/home">00 <span className='header__link--normal'>home</span></Link>
+          <Link className={`${routeName === 'destination' ? 'header__link--active' : ''} header__link`} to="/destination">01 <span className='header__link--normal'>destination</span></Link>
+          <Link className={`${routeName === 'crew' ? 'header__link--active' : ''} header__link`} to="/crew">02 <span className='header__link--normal'>crew</span></Link>
+          <Link className={`${routeName === 'technology' ? 'header__link--active' : ''} header__link`} to="/technology">03 <span className='header__link--normal'>technology</span></Link>
         </ul>
       </nav>
     </header>
