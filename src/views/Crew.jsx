266e3863a36c4e1b74a7f624f6afd49dcb218a08
DavidXploidz <david.adame.itd@gmail.com>
2023-05-30 23:14:46 -0600

    funcionalidad de crew agregada dinamicamente

--- src/views/Crew.jsx
+++ src/views/Crew.jsx
@@ -1,24 +1,51 @@
 import MainTitle from '../components/MainTitle'
 import '../styles/Crew.css'
+import { useEffect, useState } from 'react'
 
 export default function Crew() {
+
+  const [crew, setCrew] = useState([])
+  const [data, setData] = useState([])
+  const [index, setIndex] = useState(0)
+  const [tab, setTab] = useState('Douglas Hurley')
+
+  useEffect(() => {
+    consultarApi()
+  },[index])
+
+  const consultarApi = async () => {
+    const url = 'http://localhost:5173/db.json';
+    const respuesta = await fetch(url);
+    const resultado = await respuesta.json();
+    setData(resultado.crew);
+    setCrew(resultado.crew[index]);
+  }
+
+  const {name, role, bio, images} = crew;
+
+  const handleSetTab = (tab, index) => {
+    setTab(tab)
+    setIndex(index)
+  } 
+
   return (
     <div className="crew">
         <MainTitle number={'02'} title={'Meet your crew'} />
         <div className='crew__content'>
-            <p className='crew__role'>Commander</p>
-            <h3 className='crew__name'>Douglas hurley</h3>
-            <p className='crew__bio'>
-                Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.
-            </p>
+            <p className='crew__role'>{role}</p>
+            <h3 className='crew__name'>{name}</h3>
+            <p className='crew__bio'>{bio}</p>
         </div>
         <div className='crew__dots'>
-            <div className='crew__dot'></div>
-            <div className='crew__dot'></div>
-            <div className='crew__dot'></div>
-            <div className='crew__dot'></div>
+          {data?.map((person, index) => (
+            <div key={index} className={`${tab === person.name ? 'crew__dot--active' : ''} crew__dot`} onClick={() => handleSetTab(person.name, index)}></div>
+          ))}
+            {/* <div className='crew__dot crew__dot--active' onClick={() => handleSetTab(0)}></div>
+            <div className='crew__dot' onClick={() => handleSetTab(1)}></div>
+            <div className='crew__dot' onClick={() => handleSetTab(2)}></div>
+            <div className='crew__dot' onClick={() => handleSetTab(3)}></div> */}
         </div>
-        <img className='crew__image' src="/images/crew/image-douglas-hurley.webp" alt="Image person" />
+        <img className='crew__image' src={`${images?.png}`} alt={`Image profile of ${name}`} />
     </div>
   )
 }
