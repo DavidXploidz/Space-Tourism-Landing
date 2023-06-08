78c22604b5d517f18967c4626a6137939c2be72c
DavidXploidz <david.adame.itd@gmail.com>
2023-06-07 23:48:34 -0600

    fix afe

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import router from '../router'
 import { RouterProvider } from 'react-router-dom'
 import { SpaceProvider } from './context/SpaceProvider'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <SpaceProvider>
-        <RouterProvider router={router} />
-      </SpaceProvider>
-    </React.StrictMode>
-  </BrowserRouter>
+
+  <HashRouter>
+    <SpaceProvider>
+      <RouterProvider router={router} />
+    </SpaceProvider>
+  </HashRouter>
+
+
 );
