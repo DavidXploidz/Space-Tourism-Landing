7ab5abdc574b3e6bd019bfc8aed0f787ab8b3fd0
DavidXploidz <david.adame.itd@gmail.com>
2023-06-08 23:11:00 -0600

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,15 @@
-
+import React from 'react';
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import router from '../router'
-import { RouterProvider } from 'react-router-dom'
 import { SpaceProvider } from './context/SpaceProvider'
-import { HashRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-  <HashRouter>
+  <React.StrictMode>
     <SpaceProvider>
       <RouterProvider router={router} />
     </SpaceProvider>
-  </HashRouter>
-
-
+  </React.StrictMode>
 );
